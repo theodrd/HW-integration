3e8faab53b6d9fc0d82ace7888c81524c4bbbd3c
Hugo Lhuillier <hugo.lhu@gmail.com>
2017-03-09 10:31:43 +0100

    Ex. 1 final commit
    Polished answers to question 1
    Build tests for question 1

--- src/HW_int.jl
+++ src/HW_int.jl
@@ -1,4 +1,3 @@
-
 module HW_int
 
 
@@ -12,20 +11,111 @@ module HW_int
 	using Distributions
 	using LaTeXStrings
 
-	# here are some functions I defined for usage
-	# in several sub questions
+
+	# here are some functions I defined for useage in several sub questions:
 
 	# demand function
+    q(y) = 2*y^(-0.5)
 
-	# gauss-legendre adjustment factors for map change
+	# integration function
+
+  """
+  Numerical integration of functions in one dimension
+
+  #### Fields
+  - `f`: Function to integrate
+  - `p::Array{Float64}(2)`: Array containing the bounds of integration
+  - `g::String`: Integration rule to choose among {"gauss-legendre", "monte-carlo", "quasi-monte-carlo"}
+  - `n::Integer` : Number of points for the numerical integration (set equal to 1000 by default)
+  - `verbose::Bool`: Add comment (true by default)
+
+  #### Returns
+
+  Returns a numerical approximation of the value of the inetgral.
+  """
+
+  function integrate(f, p::Array, g::String; n::Int=1000, verbose::Bool=true)
+      # Define the rule of integration
+      if typeof(g) != String
+          g = "g"
+      end
+
+      if g == "gauss-legendre"
+          # if Gauss - Legendre
+          rule 		= gausslegendre(n)
+
+          # Choose the nodes
+          X 			= rule[1]
+          # Adjust for the interval of integration by a change of variable
+          X 			= (p[2]-p[1])/2 * X + (p[2] + p[1])/2
+          adj_var =  (p[2]-p[1])/2
+
+          # Choose the associate weights
+          W 			=  rule[2]
+
+
+      elseif g == "monte-carlo"
+          # if Monte-Carlo
+          rule 		= rand(n), ones(n)
+
+          # Choose the nodes
+          X 			= rule[1]
+          # Adjust for the interval of integration by a change of variable
+          X 			= p[1] +  (p[2] - p[1]) * X
+          adj_var = (p[2] - p[1]) / n
+
+          # Choose the associate weights
+          W 			=  rule[2]
+
+      elseif g == "quasi-monte-carlo"
+          # if Monte-Carlo
+          s 			= SobolSeq(1)
+          rule 		= (hcat([next(s) for i in 1:n]...)', ones(n))
+
+          # Choose the nodes
+          X 			= rule[1]
+          # Adjust for the interval of integration by a change of variable
+          X 			= p[1] +  (p[2] - p[1]) * X
+          adj_var = (p[2] - p[1]) / n
+
+          # Choose the associate weights
+          W 			=  rule[2]
+
+      else
+          error("This integration function does not support the method $g", g)
+      end
+
+      S = 0 #initialization
+
+      # approximate the integral
+      if g == "gauss-legendre"
+          for (i, x) in enumerate(X)
+              S  += W[i] * f(x)
+          end
+      else
+          S 			= sum(f, X)
+      end
+
+      # adjust for the change of variable
+      S *= adj_var
+
+      if verbose == true
+          println("Integral approximation, $g, for $n points: ", S)
+      end
+
+      return S, X
+
+  end
 
 	# eqm condition for question 2
 	# this is the equilibrium condition: total demand = supply,
 
 
 	# weighted sum for integration from the slides.
-	"""
-	Answer Question 1)a) of the homework
+
+
+  """
+  Answer Question 1)a) of the homework
 
 	#### Fields
 
@@ -34,13 +124,10 @@ module HW_int
 	#### Returns
 
 	Returns a plot with the demand function and the consumer surplus.
-
 	"""
 
-	function question_1a(p)
+  function question_1a(p)
 
-		# Define the demand function
-		q(x) = 2 * x^(-0.5)
 		x		 = linspace(5e-1, 5, 200)
 		# TODO: would like to fill the area in between the two lines
 		plot(x, q, xlab = L"p", ylab = L"q(p)", title = "Demand function")
@@ -48,7 +135,8 @@ module HW_int
 
 	end
 
-	"""
+
+  """
 	Answer Question 1)b) of the homework (Gauss-legendre approximation)
 
 	#### Fields
@@ -59,32 +147,13 @@ module HW_int
 	#### Returns
 
 	Returns a plot of the nodes vs. function value.
-
 	"""
 
 	function question_1b(n, p)
-		# Gauss-legendre integration
-		# Define the demand function
-		q(y) 			= 2 * y^(-0.5)
-		# Draw the nodes and the weight from the Guass-legendre methode
-		rule			= gausslegendre(n)
-		X 				= rule[1]
-		# Adjust to the appropriate interval
-		X					= (p[2] - p[1]) * X / 2 + (p[2] + p[1]) / 2
-		W 				= rule[2]
-		# Approximate the integral
-		S					= 0
-		for (x_idx, x) in enumerate(X)
-			S			 += W[x_idx] * q(x)
-		end
-		S				 *= (p[2] - p[1]) / 2
-		println("Integral approximation, Gauss-legendre, for $n points: ", S)
-
-		return X
-
+    	integrate(q, p, "gauss-legendre";n = n)
 	end
 
-	"""
+  """
 	Answer Question 1)c) of the homework (Monte Carlo approximation)
 
 	#### Fields
@@ -95,26 +164,15 @@ module HW_int
 	#### Returns
 
 	Returns a plot of the nodes vs. function value.
-
 	"""
+
 	function question_1c(n, p)
-		# Monte Carlo integration
-		# Define the demand function
-		q(y) 	= 2 * y^(-0.5)
-		# Draw N numbers from the uniform (0,1)
-		X			= rand(n)
-		# Adjust to the appropriate interval
-		X			= p[1] + X * (p[2] - p[1])
-		# Then approximate the integral
-		S			= sum(q, X)
-		S		 *= (p[2] - p[1]) / n
-		println("Integral approximation, Monte Carlo, for $n points: ", S)
-
-		return X
-
+        integrate(q, p, "monte-carlo"; n=n)
 	end
 
-	"""
+
+
+  """
 	Answer Question 1)d) of the homework (Quasi Monte Carlo approximation)
 
 	#### Fields
@@ -125,45 +183,30 @@ module HW_int
 	#### Returns
 
 	Returns a plot of the nodes vs. function value.
-
 	"""
 
 	function question_1d(n, p)
-		# Quasi Monte Carlo inegration. Identical to 1c but with different sampling
-		q(y)	= 2 * y^(-0.5)
-		# Draw a one-dimensional Sobol sequence, and convert it in an Array
-		X 		= SobolSeq(1)
-		X 		= hcat([next(X) for i = 1:n]...)'
-		# Adjust to the appropriate interval
-		X			= p[1] + X * (p[2] - p[1])
-		# Then approximate the integral
-		S			= sum(q, X)
-		S		 *= (p[2] - p[1]) / n
-		println("Integral approximation, Quasi Monte Carlo, for $n points: ", S)
-
-		return X
-
+        integrate(q, p, "quasi-monte-carlo"; n=n)
 	end
 
-	"""
+  """
 	Display the solutions of question 1.
 
 	#### Fields
 
-	- `n::Integer` : Number of points for the numerical integration
+	- `N::StepRange{Int64,Int64}` : Number of points for the numerical integration
 	- `p::Array{Float64}(2)` : Array containing the original and the new price.
 
 	#### Returns
 
 	Returns the approximation of the integral and a plot comparing the three methods.
 	"""
-	function show_sol_1(n,p)
+	function show_sol_1(;N::StepRange{Int64,Int64}=10:1000:10010,p = [1,4])
 
-		q(y)			= 2 * y^(-0.5)
 		x 				= linspace(5e-1, 5, 300)
-		X_gauss 	= question_1b(n,p)
-		X_monte		= question_1c(n,p)
-		X_quasi		= question_1d(n,p)
+		X_gauss 	= question_1b(N[1],p)[2]
+		X_monte		= question_1c(N[1],p)[2]
+		X_quasi		= question_1d(N[1],p)[2]
 
 		plot1			= plot(x, q, xlab = L"p", ylab = L"q(p)", title = "Gauss-Legendre")
 		vline!(p, line = 2)
@@ -175,10 +218,23 @@ module HW_int
 		vline!(p, line = 2)
 		scatter!(X_quasi, q)
 
-		l 				= @layout [a b; c]
-		return plot(plot1, plot2, plot3, layout = l)
+    #Integral approximate value for different n and the three different methods
+    mat 			= [[integrate(q, p, "gauss-legendre"; n = i, verbose = false)[1] for i in N],
+    						[integrate(q, p, "monte-carlo"; n = i, verbose = false)[1] for i in N],
+    						[integrate(q, p, "quasi-monte-carlo"; n = i, verbose = false)[1] for i in N]]
+    plot4 		= plot(N, mat, xlab = L"n", ylab = L"\int q", title="Numerical integration",
+                    label=["Gauss-Legendre" "Monte-Carlo" "Quasi-Monte-Carlo"])
 
-	end
+		l 				= @layout [a b; c; d]
+		return plot(plot1, plot2, plot3, plot4, layout = l)
+
+  end
+
+
+
+
+
+
 
 
 	function question_2a(n)
@@ -192,16 +248,19 @@ module HW_int
 
 
 	# function to run all questions
-	function runall(;n=10, p=[1,4])
+	function runall(n=10, p=[1,4])
 		println("running all questions of HW-integration:")
 		println("results of question 1:")
-		return show_sol_1(n,p)
+		question_1b(n, p)	# make sure your function prints some kind of result!
+		question_1c(n, p)
+		question_1d(n, p)
+		return show_sol_1()
 		println("")
-		# println("results of question 2:")
-		# q2 = question_2a(n)
-		# println(q2)
-		# q2b = question_2b(n)
-		# println(q2b)
+		println("results of question 2:")
+		q2 = question_2a(n)
+		println(q2)
+		q2b = question_2b(n)
+		println(q2b)
 		println("end of HW-integration")
 	end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ module IntTest
 	using HW_int
 	using Base.Test
 
-	@test 1 == 1
+	@test typeof(HW_int.question_1b(10,[1,4])) == Tuple{Float64,Array{Float64,1}}
+	@test typeof(HW_int.question_1c(10,[1,4])) == Tuple{Float64,Array{Float64,1}}
+	@test typeof(HW_int.question_1d(10,[1,4])) == Tuple{Float64,Array{Float64,2}}
 
 end
